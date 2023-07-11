c2bfae21d6cc8ce55a14787b62fad4c3162a6d0e
kuswandi-ti <kuswandi.ti@gmail.com>
2023-07-11 12:02:36 +0700

    28th Commit : Update Banner Front End

--- resources/views/frontend/pages/about/page.blade.php
+++ resources/views/frontend/pages/about/page.blade.php
@@ -2,13 +2,17 @@
     <div class="container">
         <div class="row">
             <div class="mb-3 col-lg-6 col-12 mb-lg-0">
-                <img src="{{ asset('template/frontend/images/group-people-volunteering-foodbank-poor-people.jpg') }}"
-                    class="custom-text-box-image img-fluid" alt="">
+                @if (!empty($setting))
+                    <img src="{{ url('storage' . $setting->path_image_business ?? '') }}"
+                        class="custom-text-box-image img-fluid" alt="">
+                @else
+                    <img src="{{ url(env('NO_IMAGE_SQUARE')) }}" class="custom-text-box-image img-fluid">
+                @endif
             </div>
 
             <div class="col-lg-6 col-12">
                 <div class="custom-text-box">
-                    <h2 class="mb-2">Tentang Kami</h2>
+                    <h2 class="mb-2">Tentang {{ $setting->business_name ?? '' }}</h2>
                     <h5 class="mb-3"><span class="text-secondary">{{ $setting->business_name ?? '' }}</span>,
                         {{ $setting->short_description ?? '' }}
                     </h5>
@@ -17,15 +21,15 @@
                     </p>
                 </div>
 
-                <div class="row">
-                    <div class="col-lg-6 col-md-6 col-12">
-                        <div class="custom-text-box mb-lg-0">
-                            <h5 class="mb-3">Misi Kami</h5>
-                            {!! $setting->vision ?? '' !!}
-                        </div>
-                    </div>
+                <div class="custom-text-box">
+                    <h2 class="mb-2">Misi {{ $setting->business_name ?? '' }}</h2>
+                    <p class="mb-0">
+                        {!! $setting->vision ?? '' !!}
+                    </p>
+                </div>
 
-                    <div class="col-lg-6 col-md-6 col-12">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-12">
                         <div class="flex-wrap custom-text-box d-flex d-lg-block mb-lg-0">
                             <div class="counter-thumb">
                                 <div class="d-flex">
@@ -38,10 +42,8 @@
 
                             <div class="mt-4 counter-thumb">
                                 <div class="d-flex">
-                                    <span class="counter-number" data-from="1"
-                                        data-to="{{ amount_format_id($donation_sum ?? 0) / 1000000 }}"
-                                        data-speed="1000"></span>
-                                    <span class="counter-number-text">jt</span>
+                                    <span class="counter-number-text">Rp.&nbsp;</span>
+                                    <span class="counter-number-text">{{ amount_format_id($donation_sum ?? 0) }}</span>
                                 </div>
                                 <span class="counter-text">Donasi</span>
                             </div>
